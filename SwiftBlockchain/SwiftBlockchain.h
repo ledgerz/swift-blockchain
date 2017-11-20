d3ca4566b394ae21329730b8b2fed84f8a45ca69
Christian Haug <csh@christianhaug.com>
2017-11-20 13:45:59 +0100

    Initial Commit

---
+++ SwiftBlockchain/SwiftBlockchain.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBlockchain.h
+//  SwiftBlockchain
+//
+//  Created by Chris on 20.11.17.
+//  Copyright © 2017 csh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftBlockchain.
+FOUNDATION_EXPORT double SwiftBlockchainVersionNumber;
+
+//! Project version string for SwiftBlockchain.
+FOUNDATION_EXPORT const unsigned char SwiftBlockchainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBlockchain/PublicHeader.h>
+
+
